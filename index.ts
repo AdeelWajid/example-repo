81f0e4203a1b4101503fc3dbbcf54fc29c486c37
AdeelWajid <adeelwajidbn@gmail.com>
2024-11-18 20:53:35 +0500

    asd

--- index.ts
+++ index.ts
@@ -11,25 +11,29 @@ EventsSDK.on("GameStarted", () => {
 		return
 	}
 	
-	const mainMenu = MenuManager.AddEntry("Kill Steal Script")
-	toggleButton = mainMenu.AddToggle("Enable Kill Steal", false, "K")
-	EventsSDK.on("Draw", () => {
-		const myHero = LocalPlayer?.Hero
-		if (myHero === undefined) {
-			return
-		}
 	
-		// Check if hero is visible by enemies
-		if (myHero.IsVisibleForEnemies_) {
-			console.log("Visible by enemies!")
-		}
-	
-		// Kill steal logic
-		if (MenuManager && toggleButton?.value) {
-			console.log("Kill steal is enabled!")
-		}
-	})
 })
 
+const VISIBLE_TEXT = "VISIBLE BY ENEMY!"
+const TEXT_POSITION = new Vector2(400, 100)
 
+EventsSDK.on("Draw", () => {
+	const myHero = LocalPlayer?.Hero
+	if (myHero === undefined) {
+		return
+	}
 
+	// Check if hero is visible by enemies
+	if (myHero.IsVisibleForEnemies_) {
+		console.log("Visible by enemies!")
+	}
+	if(MenuManager.IsOpen){
+		const mainMenu = MenuManager.AddEntry("Kill Steal Script")
+	toggleButton = mainMenu.AddToggle("Enable Kill Steal", false, "K")
+	if (MenuManager && toggleButton?.value) {
+		console.log("Kill steal is enabled!")
+	}
+	}
+	// Kill steal logic
+	
+})
