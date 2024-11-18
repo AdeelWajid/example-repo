00e8fed2a4529cde066fdcabe6d91915ba560533
AdeelWajid <adeelwajidbn@gmail.com>
2024-11-18 20:48:25 +0500

    asd

--- index.ts
+++ index.ts
@@ -3,10 +3,17 @@ import { EventsSDK } from "github.com/octarine-public/wrapper/index"
 import { LocalPlayer } from "github.com/octarine-public/wrapper/wrapper/Objects/Base/Entity"
 import { Vector2 } from "github.com/octarine-public/wrapper/wrapper/Base/Vector2"
 
-// Create main menu
-const mainMenu = MenuManager.AddEntry("Kill Steal Script")
-const toggleButton = mainMenu.AddToggle("Enable Kill Steal", false, "K")
+let toggleButton: any
 
+// Create main menu
+EventsSDK.on("GameStarted", () => {
+	if (!MenuManager) {
+		return
+	}
+	
+	const mainMenu = MenuManager.AddEntry("Kill Steal Script")
+	toggleButton = mainMenu.AddToggle("Enable Kill Steal", false, "K")
+})
 
 const VISIBLE_TEXT = "VISIBLE BY ENEMY!"
 const TEXT_POSITION = new Vector2(400, 100)
@@ -23,11 +30,7 @@ EventsSDK.on("Draw", () => {
 	}
 
 	// Kill steal logic
-	if (toggleButton.value) {
+	if (MenuManager && toggleButton?.value) {
 		console.log("Kill steal is enabled!")
-		// console.log(`Using stun: ${useStun.value}`)
-		// console.log(`Using nuke: ${useNuke.value}`)
-		// console.log(`HP threshold: ${minHealth.value}`)
-		// Your kill steal logic here
 	}
 })
