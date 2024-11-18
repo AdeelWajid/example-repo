8960436eee776a55bedd70dad98b9c78af4b32c8
AdeelWajid <adeelwajidbn@gmail.com>
2024-11-18 20:24:33 +0500

    asd

--- index.ts
+++ index.ts
@@ -1,23 +1,5 @@
-
-// import { Color } from "github.com/octarine-public/wrapper/wrapper/Base/Color"
 import { EventsSDK } from "github.com/octarine-public/wrapper/index"
-import { LocalPlayer } from "github.com/octarine-public/wrapper/wrapper/Objects/Base/Entity"
-// import { Vector2 } from "github.com/octarine-public/wrapper/wrapper/Base/Vector2"
-
-// const VISIBLE_TEXT = "VISIBLE BY ENEMY!"
-// const TEXT_COLOR = new Color(255, 0, 0, 255) // Red color
-// const TEXT_POSITION = new Vector2(400, 100) // Position on screen
-
-EventsSDK.on("Draw", () => {
-	const myHero = LocalPlayer?.Hero
-	if (myHero === undefined) {
-		return
-	}
-
-	// Check if hero is visible by enemies
-	if (myHero.IsVisible) {
-		// Draw warning text on screen
-		console.log("Hero is visible by enemies!")
-		// myHero.DrawText(TEXT_POSITION, VISIBLE_TEXT, TEXT_COLOR)
-	}
+console.log("Hello world!")
+EventsSDK.on("GameStarted", () => {
+	console.log("Hello world!")
 })
